87bf17c28878b8ce3da4a2a344af528fbc613f56
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2025-09-09 22:00:36 +0530

    dev: added modal

--- src/components/Client/ClientHead/ClientHead.tsx
+++ src/components/Client/ClientHead/ClientHead.tsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import ClientModal from "../ClientModal/ClientModal";
 
 const ClientHead = () => {
-  return (
-    <div className="flex items-center justify-between w-full mb-6">
-      <div>
-        <h1 className="text-2xl font-bold text-white">Clients</h1>
-        <p className="text-gray-400">
-          Manage your client list and add new ones here.
-        </p>
-      </div>
+    const [clientModal, setClientModal] = useState(false);
 
-    <button className="px-4 py-2 rounded-xl bg-white text-black text-sm font-medium 
-                             shadow-md hover:opacity-90 transition">
-            Add Client
-          </button>
-    </div>
-  );
+    return (
+        <div className="flex items-center justify-between w-full mb-6">
+            <div>
+                <h1 className="text-2xl font-bold text-white">Clients</h1>
+                <p className="text-gray-400">
+                    Manage your client list and add new ones here.
+                </p>
+            </div>
+
+            <button className="px-4 py-2 rounded-xl bg-white text-black text-sm font-medium 
+                             shadow-md hover:opacity-90 transition"
+                onClick={() => { setClientModal(true) }}
+            >
+                Add Client
+            </button>
+
+
+            {clientModal &&
+                <ClientModal onClose={() => { setClientModal(false) }} />
+            }
+        </div>
+    );
 };
 
 export default ClientHead;
--- src/pages/clients/index.tsx
+++ src/pages/clients/index.tsx
@@ -3,11 +3,15 @@ import ClientsTable from '@/components/Client/ClientsTable/ClientsTable'
 import React from 'react'
 
 const client = () => {
+
   return (
     <div className=" p-6 space-y-6">
       
       <ClientHead/>
+
       <ClientsTable/>
+
+
     </div>
   )
 }
